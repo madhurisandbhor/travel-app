04dc3d2e786eb419ea053d06ceef2ac60c12658b
Madhuri Sandbhor <madhuri.s@ignitiv.com>
2021-08-11 18:01:23 +0200

    react-responsive-carousel added, css changes

--- src/containers/AppHeader/Toolbar.js
+++ src/containers/AppHeader/Toolbar.js
@@ -1,59 +1,77 @@
-import React from 'react';
-import styled from 'styled-components';
-
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
-import Badge from '@material-ui/core/Badge';
-import ItineraryDrawer from './ItineraryDrawer';
+import React from 'react'
+import styled from 'styled-components'
+import clsx from 'clsx'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/IconButton'
+import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff'
+import Badge from '@material-ui/core/Badge'
+import ItineraryDrawer from './ItineraryDrawer'
 
 const ToolbarWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    position: relative;
-`;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`
 
 const useStyles = makeStyles((theme) => ({
-    hide: {
-        display: 'none',
-    },
-    open: {
-        position: 'absolute',
-        top: '-2.5rem',
-        right: '2rem',
-    },
-    customBadge: {
-        backgroundColor: theme.palette.primary.main,
-        color: "white",
-    },
-}));
+  button: {
+    borderRadius: '0.5rem',
+    padding: '0.8rem',
+    '&:hover':{
+        color: theme.palette.secondary.main,
+        backgroundColor: theme.app.white,
+    }
+  },
+  hide: {
+    display: 'none',
+  },
+  open: {
+    position: 'absolute',
+    right: '2rem',
+  },
+  customBadge: {
+    backgroundColor: theme.palette.primary.main,
+    color: 'white',
+  },
+  title: {
+    marginLeft: '1rem',
+  },
+}))
 
 const Toolbar = ({ notificationToggle, handleDrawerOpen, open, ...rest }) => {
-    const classes = useStyles();
+  const classes = useStyles()
 
-    return (
-        <ToolbarWrapper>
-            <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="end"
-                onClick={handleDrawerOpen}
-                className={clsx(open && classes.hide, !open && classes.open)}
-            >
-                <Badge
-                    variant="dot"
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                    }}
-                    classes={{ badge: notificationToggle && classes.customBadge }}
-                >
-                    <FlightTakeoffIcon />
-                </Badge>
-            </IconButton>
-            <ItineraryDrawer open={open} {...rest} />
-        </ToolbarWrapper >);
-};
+  return (
+    <ToolbarWrapper>
+      <Button
+        variant="outlined"
+        color="inherit"
+        aria-label="open drawer"
+        edge="end"
+        onClick={handleDrawerOpen}
+        className={clsx(
+          open && classes.hide,
+          !open && classes.open,
+          classes.button
+        )}
+      >
+        <Badge
+          variant="dot"
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          classes={{ badge: notificationToggle && classes.customBadge }}
+        >
+          <FlightTakeoffIcon />
+        </Badge>
+        <span className={classes.title}>Travel Itinerary</span>
+      </Button>
+      <ItineraryDrawer open={open} {...rest} />
+    </ToolbarWrapper>
+  )
+}
 
-export default React.memo(Toolbar);
+export default React.memo(Toolbar)
--- src/containers/Dashboard/Index.js
+++ src/containers/Dashboard/Index.js
@@ -1,87 +1,63 @@
-import React, { useState, useEffect, useCallback, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import asia from 'images/Asia.jpg';
-import africa from 'images/Africa.jpg';
-import antarctica from 'images/Antarctica.jpg';
-import europe from 'images/Europe.jpg';
-import oceania from 'images/Oceania.jpg';
-import north_america from 'images/North America.jpg';
-import south_america from 'images/South America.jpg';
-import ContinentName from './ContinentName';
-import ImageContainer from './ImageContainer';
-import LoadingIndicator from 'components/LoadingIndicator';
-import { InfoContext } from 'app/InfoContext';
-import { useHttp } from './useHttp';
+import React, { useState, useEffect, useCallback, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import asia from 'images/Asia.jpg'
+import africa from 'images/Africa.jpg'
+import antarctica from 'images/Antarctica.jpg'
+import europe from 'images/Europe.jpg'
+import oceania from 'images/Oceania.jpg'
+import north_america from 'images/North America.jpg'
+import south_america from 'images/South America.jpg'
+import LoadingIndicator from 'components/LoadingIndicator'
+import { InfoContext } from 'app/InfoContext'
+import { useHttp } from './useHttp'
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '80%',
-        margin: '0 auto',
-        height: '45rem'
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '100%',
+    margin: '0 auto',
+    height: '100%',
+  },
+  hidden: {
+    visibility: 'hidden',
+  },
+  sliderContainer: {
+    height: '100%',
+    width: '100%',
+    '& > .carousel-root': {
+      height: '100%',
+      '& > .carousel.carousel-slider': {
+        height: '100%',
+      },
     },
-    message: {
-        margin: '1.5rem 0',
-        textAlign: 'center',
-    },
-    hidden: {
-        visibility: 'hidden',
-    },
-    sliderContainer: {
-        height: '44rem',
-        width: '100%',
-        position: 'relative',
-        overflow: 'hidden',
-        textAlign: 'center'
-    },
-    menu: {
-        position: 'absolute',
-        left: 0,
-        zIndex: 500,
-        width: '100%',
-        bottom: 0,
-    },
-    label: {
-        cursor: 'pointer',
-        display: 'inline-block',
-        width: '1.6rem',
-        height: '1.6rem',
-        background: theme.app.white,
-        borderRadius: '5rem',
-        margin: '0 .2rem 1rem',
-        '&:hover': {
-            background: theme.palette.primary.main,
-        }
-    },
-    // slide: {
-    //     position: 'absolute',
-    //     top: 0,
-    //     left: '100%',
-    //     zIndex: 10,
-    //     backgroundPosition: '50% 50%',
-    //     transition: 'left 0s .75s',
-    // },
-    // TODO: fix syntax issue with sibling
-    // '@global': {
-    //     '[id^="slide"]:checked ~$slide': {
-    //         left: 0,
-    //         zIndex: 100,
-    //         transition: 'left .65s ease-out',
-    //     }
-    // },
-}));
+  },
+  img: {
+    width: '100%',
+    height: 'calc(100vh - 5.5rem)',
+    objectFit: 'cover',
+  },
+}))
 
-const images = [asia, africa, antarctica, europe, north_america, oceania, south_america];
+const images = [
+  asia,
+  africa,
+  antarctica,
+  europe,
+  north_america,
+  oceania,
+  south_america,
+]
 
 const Dashboard = () => {
-    const classes = useStyles();
-    const [continentsList, setContinentsList] = useState([]);
-    const [UpdatedcontinentsList, setUpdatedContinentsList] = useState([]);
-    const [selected, setSelected] = useState(0);
-    const { info, setInfo } = useContext(InfoContext);
-    const history = useHistory();
-    const url = 'https://api.everbase.co/graphql?apikey=your_key';
-    const query = `{
+  const classes = useStyles()
+  const [continentsList, setContinentsList] = useState([])
+  const [UpdatedcontinentsList, setUpdatedContinentsList] = useState([])
+  const { info, setInfo } = useContext(InfoContext)
+  const history = useHistory()
+  const url = 'https://api.everbase.co/graphql?apikey=your_key'
+  const query = `{
         continents {
             id
             name
@@ -90,81 +66,79 @@ const Dashboard = () => {
               id
             }
         }
-    }`;
+    }`
 
-    const { isLoading, data, error } = useHttp(url, query);
+  const { isLoading, data, error } = useHttp(url, query)
 
-    const getCountinentInfo = useCallback(continentSelected => {
-        setInfo({
-            ...info,
-            continentSelected: continentSelected,
-        });
-        history.push(`/continents/${continentSelected.name}`);
-    }, [history, info, setInfo]);
+  const getContinentInfo = useCallback(
+    (continentSelected) => {
+      setInfo({
+        ...info,
+        continentSelected: continentSelected,
+      })
+      history.push(`/continents/${continentSelected.name}`)
+    },
+    [history, info, setInfo]
+  )
 
-    const onRadioInput = index => {
-        setSelected(index);
-    };
+  useEffect(() => {
+    if (data !== null) {
+      setContinentsList(data.continents)
+    }
+  }, [data])
 
-    useEffect(() => {
-        if (data !== null) {
-            setContinentsList(data.continents);
-        }
-    }, [data]);
+  useEffect(() => {
+    //update list with respective images as images are not provided by backend
+    const tempList = [...continentsList]
+    const listWithImages = tempList.map((continent) => {
+      images.length > 0 &&
+        images.forEach((image) => {
+          if (
+            image.includes('/') &&
+            image.split('/')[3].toString().includes(continent.name)
+          )
+            continent.img = image
+        })
+      return continent
+    })
+    setUpdatedContinentsList(listWithImages)
+  }, [continentsList])
 
-    useEffect(() => {
-        //update list with respective images as images are not provided by backend
-        const tempList = [...continentsList];
-        const listWithImages = tempList.map(continent => {
-            images.length > 0 && images.forEach(image => {
-                    if (image.includes('/') && image.split('/')[3].toString().includes(continent.name))
-                        continent.img = image;
-            });
-            return continent;
-        });
-        setUpdatedContinentsList(listWithImages);
-    }, [continentsList]);
+  return (
+    <div className={classes.root}>
+      {isLoading && <LoadingIndicator />}
+      {error && <div>{error}</div>}
+      {!isLoading && (
+        <div className={classes.sliderContainer}>
+          <Carousel
+            autoPlay
+            infiniteLoop
+            animationHandler="fade"
+            showThumbs={false}
+            showArrows={true}
+          >
+            {UpdatedcontinentsList.length > 0 &&
+              UpdatedcontinentsList.map((continent, index) => (
+                <div
+                  key={continent.id}
+                  role="button"
+                  onClick={(event) => getContinentInfo(continent, event)}
+                >
+                  <img
+                    className={classes.img}
+                    src={continent.img}
+                    alt={continent.name}
+                  />
+                  <div className="legend">{continent.name}</div>
+                </div>
+              ))}
+          </Carousel>
+        </div>
+      )}
+    </div>
+  )
+}
 
-    return (
-        <div className={classes.root}>
-            {isLoading && <LoadingIndicator />}
-            {error && <div>{error}</div>}
-            {!isLoading &&
-                <>
-                    <div className={classes.sliderContainer}>
-                        <div className={classes.menu}>
-                            {UpdatedcontinentsList.length > 0 && UpdatedcontinentsList.map((continent, index) =>
-                                <label className={classes.label} key={continent.id} htmlFor={`slide-dot-${index + 1}`}></label>
-                            )}
-                        </div>
-                        {UpdatedcontinentsList.length > 0 && UpdatedcontinentsList.map((continent, index) =>
-                            <React.Fragment key={continent.id}>
-                                <input
-                                    id={`slide-dot-${index + 1}`}
-                                    className={classes.hidden}
-                                    type="radio"
-                                    name="slides"
-                                    onChange={e => onRadioInput(index, e)}
-                                    checked={selected === index}
-                                />
-                                <ImageContainer
-                                    className="slide"
-                                    image={`'${continent.img}'`}
-                                    onClick={(event) => getCountinentInfo(continent, event)}
-                                >
-                                    <ContinentName>{continent.name}</ContinentName>
-                                </ImageContainer>
-                            </React.Fragment>
-                        )}
-                    </div>
-                    <h4 className={classes.message}>
-                        Please click on the image to select a continent
-                    </h4>
-                </>}
-        </div >
-    );
-};
+Dashboard.propTypes = {}
 
-Dashboard.propTypes = {};
-
-export default Dashboard;
+export default Dashboard
